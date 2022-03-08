7f61889790a936f08a12b4d47a362fd963ad7a11
Asim Riaz <asimriaz@szabist.net.pk>
2022-03-08 16:10:37 +0500

    Lecture-3

---
+++ Lecture-3/mongoDb/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose')
+const db = require('../models');
+
+/* GET home page. */
+router.get('/', async function(req, res) {
+  const courses = await db.Course.find();
+  res.render('index', { 
+      title: 'Express', 
+      courses
+    });
+});
+
+module.exports = router;
