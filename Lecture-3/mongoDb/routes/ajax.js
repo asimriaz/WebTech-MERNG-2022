209315b06c80f80c5a4d85462346c07df215b3f0
Asim Riaz <asimriaz@szabist.net.pk>
2022-03-15 16:22:17 +0500

    Lecture-4

--- Lecture-3/mongoDb/routes/ajax.js
+++ Lecture-3/mongoDb/routes/ajax.js
@@ -3,26 +3,27 @@ var router = express.Router();
 const mongoose = require('mongoose')
 const db = require('../models');
 
-/* GET home page. */
-router.get('/', async function(req, res) {
-  const courses = await db.Course.find().sort({courseid: 1});
-  res.render('index', { 
-      title: 'Express', 
-      courses
-    });
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('ajax/index', { title: 'AJAX' });
 });
 
 /* GET home page. */
-router.get('/Edit/:id', async function(req, res) {
-  console.log(`courseid >> ${req.params.id}`)
-  const course = await db.Course.find({courseid: req.params.id})
-  console.log(course[0])
-  res.render('edit', { title: 'Edit', course: course[0] });
+router.get('/xhr-list', async function(req, res) {
+  const courses = await db.Course.find().sort({courseid: 1});
+  res.render('ajax/list', { title: 'Express', courses });
+})
+
+/* GET home page. */
+router.get('/xhr-edit', async function(req, res) {
+  const course = await db.Course.find({courseid: req.query.courseid});
+  res.render('ajax/edit', { title: 'Express', course: course[0] });
 })
 
 
+
 /* POST Save. */
-router.post('/Save', async function(req, res) {
+router.post('/xhr-save', async function(req, res) {
   console.log(req.body)
   const result = await db.Course.updateOne({courseid: req.body.courseid}, 
     {
@@ -30,12 +31,14 @@ router.post('/Save', async function(req, res) {
     }
   )
   console.log(result);
+  res.send(result);
   //res.render('index', { title: 'Express' });
-  res.redirect('/')
+  // res.redirect('/')
 })
+
+
 // /* GET home page. */
 // router.get('/', async function(req, res) {
 //   res.render('index', { title: 'Express' });
 // })
-
 module.exports = router;
