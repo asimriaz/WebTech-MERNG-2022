8cee2270c55593b1eac4a8d584244ced2d3a0b45
Asim Riaz <asimriaz@szabist.net.pk>
2022-03-22 15:10:08 +0500

    Lecture-5 Part 1

--- Lecture-3/mongoDb/app.js
+++ Lecture-3/mongoDb/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var spaRouter = require('./routes/ajax');
+var restRouter = require('./routes/rest');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/SPA', spaRouter);
+app.use('/REST', restRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
