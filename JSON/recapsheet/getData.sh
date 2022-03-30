70071b70f781e9967cb16f0da54855bc9534145a
Asim Riaz <asim.riaz@gmail.com>
2022-03-30 19:31:58 +0500

    Update getData.sh

--- JSON/recapsheet/getData.sh
+++ JSON/recapsheet/getData.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-for collection in $(ls *.json | cut -d'.' -f1); do docker exec -i mongo-db sh -c "mongoimport -d recapsheet -c ${collection} --jsonArray" < ${collection}.json; done
+if [[ $# -eq 0 ]] ; then
+    echo 'Please give database name'
+    exit 1
+fi
+
+for collection in $(ls *.json | cut -d'.' -f1); do docker exec -i mongo-db sh -c "mongoimport -d ${1} -c ${collection} --jsonArray" < ${collection}.json; done
