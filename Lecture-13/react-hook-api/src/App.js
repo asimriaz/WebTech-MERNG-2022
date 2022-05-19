d96130a6018d8fcc8bf137866dfe197f9633ccb4
Asim Riaz <asim.riaz@gmail.com>
2022-05-19 11:10:26 +0500

    Lecture-13

---
+++ Lecture-13/react-hook-api/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HkStudentList from './components/HkStudentList';
+
+function App() {
+  return (
+    <div>
+      {/* <StudentList /> */}
+      <HkStudentList />
+    </div>
+  );
+}
+
+export default App;
