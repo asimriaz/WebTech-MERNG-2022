ec7c4a3e4760263df5ac14fa84c3626f5c61a8ce
Asim Riaz <asim.riaz@gmail.com>
2022-05-30 13:15:05 +0500

    Lecture-13

--- Lecture-13/react-routes/src/components/ContactUs.js
+++ Lecture-13/react-routes/src/components/ContactUs.js
@@ -1,13 +1,9 @@
 import React from 'react'
 
 const ContactUs = () => {
-    return (
-        <div>
-            <h3>
-                Contact Us
-            </h3>
-        </div>
-    )
+  return (
+    <div>ContactUs</div>
+  )
 }
 
 export default ContactUs
